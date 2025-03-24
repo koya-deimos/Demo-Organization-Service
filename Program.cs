69713b02333209eeb636763b0c01b80d162a2e01
Olamide Onakoya <123734265+koya-deimos@users.noreply.github.com>
2025-03-24 14:48:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,11 +9,9 @@ builder.Services.AddLogging();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 var summaries = new[]
 {
