f59bc976b672f1ede753d5ce266c952feef955ca
Olamide Onakoya <olamide@salus.cloud>
2025-11-01 18:45:50 +0100

    add migration

---
+++ Migrations/20251101174318_AddOrganization.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrganization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Organizations",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("c2a9133d-3e52-4c90-bb9d-bbc7b5284b44"), "Track your fitness and receive notifications.", "Smartwatch Organization" },
+                    { new Guid("d7b8b57c-5d7b-4e8a-9a9c-b2b7d2b21d9f"), "Amazing Organization", "KoyaOrg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
