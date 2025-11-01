b1d5840dd2ca0da993fb0da49b07df73cf791bf5
Olamide Onakoya <olamide@salus.cloud>
2025-11-01 19:07:17 +0100

    cleanup

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace webapi.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.7")
+                .HasAnnotation("ProductVersion", "8.0.21")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
